34ed33d66caf1bfb4e1ebdcc57b7cce8cf1e9496
pranayk11 <131856262+pranayk11@users.noreply.github.com>
2023-04-26 19:49:21 +0530

    Add files via upload

---
+++ Day 88 - Todo List/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField, PasswordField
+from wtforms.validators import DataRequired
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign In")
+
