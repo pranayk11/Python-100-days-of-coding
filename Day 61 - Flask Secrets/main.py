2b463c7c6af37bd84743aa6a51ac37dfbc62f0bc
pranayk11 <131856262+pranayk11@users.noreply.github.com>
2023-04-26 20:31:59 +0530

    Create main.py

---
+++ Day 61 - Flask Secrets/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = "any_string_you_want_just_keep_it_secret"
+
+# https://wtforms.readthedocs.io/en/2.3.x/validators/#module-wtforms.validators
+# Email() requires WTForms email_validator package:
+#       pip install wtforms[email]
+# or    pip install email-validator
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Log In')
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
